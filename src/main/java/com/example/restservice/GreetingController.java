11899e2747ba02aa24aecf197905bad629de9b80
Lee Jia Keat <leejiakeat@gmail.com>
2023-06-16 13:08:43 +0800

    wip

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,7 +1,12 @@
 package com.example.restservice;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +20,17 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/locations")
+    public String getHousingLocations() throws IOException {
+        Resource resource = new ClassPathResource("static/housing-locations.json");
+        InputStream inputStream = resource.getInputStream();
+
+        byte[] bytes = inputStream.readAllBytes();
+        String json = new String(bytes);
+
+        return json;
+    }
+
 }
