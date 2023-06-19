a71912b877e44ee7d95c7579e505140406014474
Lee Jia Keat <leejiakeat@gmail.com>
2023-06-20 01:44:22 +0800

    pog

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Read environment variables from .env file
-FOR /F "tokens=*" %%G IN (.env) DO SET %%G
-docker run -e DB_USER=%DB_USER% -e DB_PASS=%DB_PASS% ksxjltze/spring-booter
+FOR /F "tokens=*" %%G IN (env.properties) DO SET %%G
+docker run --name spring-booter -p 8080:8080 -e DB_USER=%DB_USER% -e DB_PASSWORD=%DB_PASSWORD% -e DB_DATABASE=%DB_DATABASE% ksxjltze/spring-booter
